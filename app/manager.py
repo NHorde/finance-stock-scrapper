5c7cb60c19055c396e746566babb5f798a0426a2
NHorde <enhorde@gmail.com>
2020-07-07 20:32:52 -0400

    Adding **kwargs arguments to class object

--- app/manager.py
+++ app/manager.py
@@ -1,12 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from json import loads
+
 from state import State
 
 def get_html(state: State):
     soup = BeautifulSoup(
-        requests.get("https://finance.yahoo.com/quote/%s/key-statistics?p=%s" % (self.ticker, self.ticker)).content,
-        'lxml')
+        # requests.get("https://finance.yahoo.com/quote/%s/key-statistics?p=%s" % (self.ticker, self.ticker)).content, 'lxml')
+        requests.get("https://finance.yahoo.com/quote/%s/key-statistics?p=%s" % ("AMZN", "AMZN")).content, "lxml")
     script = soup.find("script", text=re.compile("root.App.main")).text
     data = loads(re.search("root.App.main\s+=\s+(\{.*\})", script).group(1))
     state.url = data['context']['dispatcher']['stores']
+    print(state.url)
+    return state.url
 
 def manager(state: State):
     """  
@@ -21,9 +28,10 @@ def manager(state: State):
     except:
         result = "nice try"
         print("not good")
+    print(result)
     return result
 
-state = State()
-print(state.url)
+state = State(test = 'test')
+get_html(state)
 # manager(state = state)
 
--- app/state.py
+++ app/state.py
@@ -1,7 +1,4 @@
-from bs4 import BeautifulSoup
-import requests
-import re
-from json import loads
+
 
 
 class State:
@@ -9,8 +6,9 @@ class State:
         'ticker': None
     }
 
-    def __init__(self, **kwargs):
-        self.__dict__.update(self._defaults)
-        self.__dict__.update(kwargs)
-
-
+    def __init__(self, test, **kwargs):
+        """
+        :param event:
+        :type even: dict
+        """
+        self.test = kwargs.get("test", "test")
