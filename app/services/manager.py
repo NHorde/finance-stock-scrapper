134f06b0366b736b606d7b8bec6526177acb5e2f
NHorde <enhorde@gmail.com>
2020-07-07 23:18:01 -0400

    Adding financial information

--- app/services/manager.py
+++ app/services/manager.py
@@ -4,6 +4,7 @@ import sys
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 from json import loads
 
 PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -11,21 +12,31 @@ sys.path.append(PATH)
 
 from libs.state import State
 
+
 def get_html(state: State):
-    soup = BeautifulSoup(
-        # requests.get("https://finance.yahoo.com/quote/%s/key-statistics?p=%s" % (self.ticker, self.ticker)).content, 'lxml')
-        requests.get("https://finance.yahoo.com/quote/%s/key-statistics?p=%s" % ("AMZN", "AMZN")).content, "lxml")
+    soup = BeautifulSoup(requests.get("https://finance.yahoo.com/quote/%s/key-statistics?p=%s" % ("AMZN", "AMZN")).content, "lxml")
     script = soup.find("script", text=re.compile("root.App.main")).text
     data = loads(re.search("root.App.main\s+=\s+(\{.*\})", script).group(1))
     state.url = data['context']['dispatcher']['stores']
-    return crawler_summary(state=state)
+    print("good")
+    return crawler_quote_summary(state=state)
 
-def crawler_summary(state: State):
+
+def crawler_quote_summary(state: State):
     try:
         state.quote_summary_store = state.url['QuoteSummaryStore']
-    except ValueError("No data"):
+    except ValueError:
         state.quote_summary_store = None
-    print(state.quote_summary_store)
+    return crawler_financial_data(state=state)
+
+    return state
+def crawler_financial_data(state: State):
+    try:
+        state.financial_data = state.quote_summary_store['financialData']
+    except ValueError:
+        state.financial_data = None
+    return state
+
 
 def manager(state: State):
     """
@@ -45,5 +56,6 @@ def manager(state: State):
 
 
 state = State()
-
-manager(state = state)
+state = manager(state = state)
+print(state.financial_data)
+# print(json.dumps(state.financial_data, indent=3, default=str))
