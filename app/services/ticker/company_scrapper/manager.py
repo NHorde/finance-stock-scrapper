d69e92776b773e90f1c831474ae7c4393988596f
NHorde <enhorde@gmail.com>
2020-07-09 22:38:35 -0400

    Add ingestor

---
+++ app/services/ticker/company_scrapper/manager.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+def download_company_list():
+    url_nasdaq = "https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download"
+    url_amex = "https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download"
+    url_nyse = "https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download"
+
+    col = ["Symbol", "Name", "MarketCap","IPOyear", "Sector", "industry"]
+    df_nasdaq = pd.read_csv(url_nasdaq, usecols = col)
+    df_amex = pd.read_csv(url_amex, usecols=col)
+
+    df_nyse = pd.read_csv(url_nyse, usecols=col)
+    df = pd.concat([df_nasdaq, df_amex, df_nyse])
+
+    PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    print(PATH)
+    df.to_csv('test.csv')
+
+download_company_list()
+
