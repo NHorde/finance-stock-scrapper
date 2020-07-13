b3d36850e0ae6bd56bd2fa9d2393d3cb98379c33
NHorde <enhorde@gmail.com>
2020-07-12 22:59:54 -0400

    Start of batch process

--- app/services/manager.py
+++ app/services/manager.py
@@ -1,3 +1,6 @@
+import os
+import pandas as pd
+
 from services.financial_scrapper.manager import manager as manager_financial_scrapper
 from services.company_scrapper.manager import manager as manager_company_scrapper
 
@@ -5,17 +8,21 @@ from libs.state import State
 
 def get_company_list(state: State):
     try:
-        df = manager_company_scrapper()
+        manager_company_scrapper()
         print("Company list scrapped with success")
     except:
         print("Company list not successfully scrapped")
-    return df, get_ticker_test(state = state)
+    return get_ticker_information(state = state)
 
 
-def get_ticker_test(state: State):
+def get_ticker_information(state: State):
+    # PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    # df = pd.read_csv(PATH + '/app/data/ticker_list.csv')
+    # print(df.head())
     state = manager_financial_scrapper(state = state)
     print(state.status)
     return state
+
 
 def manager():
     state = State()
