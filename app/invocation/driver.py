499c83fbd00fb86d22cc8aa356201701b26d0509
NHorde <enhorde@gmail.com>
2020-07-12 22:28:47 -0400

    Refactoring driver file, still bug in master manager

--- app/invocation/driver.py
+++ app/invocation/driver.py
@@ -1,11 +1,8 @@
 import setup
-#
-# from services.ticker.financial_scrapper.manager import manager as manager_financial_scrapper
-# from services.ticker.company_scrapper.manager import manager as manager_company_scrapper
+from libs.state import State
 
-# from libs.state import State
 
-# from libs.manager import
-# manager_company_scrapper()
-# state = manager_financial_scrapper(state = State())
-# print(state.status)
+from services.manager import manager as manager_services
+
+
+manager_services(state = State())
