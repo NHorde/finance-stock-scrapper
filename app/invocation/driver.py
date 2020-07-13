439c27eeb727dcb64f303d6380aa01e86d08f5fa
NHorde <enhorde@gmail.com>
2020-07-12 22:22:42 -0400

    Manager for lib

--- app/invocation/driver.py
+++ app/invocation/driver.py
@@ -1,11 +1,11 @@
 import setup
+#
+# from services.ticker.financial_scrapper.manager import manager as manager_financial_scrapper
+# from services.ticker.company_scrapper.manager import manager as manager_company_scrapper
 
-from services.ticker.financial_scrapper.manager import manager as manager_financial_scrapper
-from services.ticker.company_scrapper.manager import manager as manager_company_scrapper
+# from libs.state import State
 
-from libs.state import State
-
-
-manager_company_scrapper()
-state = manager_financial_scrapper(state = State())
-print(state.status)
+# from libs.manager import
+# manager_company_scrapper()
+# state = manager_financial_scrapper(state = State())
+# print(state.status)
--- app/services/manager.py
+++ app/services/manager.py
@@ -1,11 +1,22 @@
-import setup
-
 from services.financial_scrapper.manager import manager as manager_financial_scrapper
 from services.company_scrapper.manager import manager as manager_company_scrapper
 
 from libs.state import State
 
+def get_company_list():
+    try:
+        manager_company_scrapper()
+        print("Company list scrapped with success")
+    except:
+        print("Company list not successfully scrapped")
+    return get_ticker_test(state = state)
 
-manager_company_scrapper()
-state = manager_financial_scrapper(state = State())
-print(state.status)
+
+def get_ticker_test(state : State):
+    state = manager_financial_scrapper(state = State())
+    print(state.status)
+    return test
+
+def manager(state: State):
+    get_company_list()
+
