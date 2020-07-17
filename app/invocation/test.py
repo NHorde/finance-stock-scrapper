93162e455fd4ce0cb3772e0d6d9abe85f2af2d45
NHorde <enhorde@gmail.com>
2020-07-17 00:38:35 -0400

    Need to debug company scrapper

--- app/invocation/test.py
+++ app/invocation/test.py
@@ -1,12 +1,17 @@
 import time
 import timeout_decorator
+import pandas as pd
 
-@timeout_decorator.timeout(5)
-def mytest():
-    print ("Start")
-    for i in range(1,10):
-        time.sleep(1)
-        print ("%d seconds have passed" % i)
+# @timeout_decorator.timeout(5)
+# def mytest():
+#     print ("Start")
+#     for i in range(1,10):
+#         time.sleep(1)
+#         print ("%d seconds have passed" % i)
+#
+# if __name__ == '__main__':
+#     mytest()
 
-if __name__ == '__main__':
-    mytest()
+col = ["Symbol", "Name", "MarketCap", "IPOyear", "Sector", "industry"]
+df = pd.read_csv("https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download", usecols = col)
+print(df.head())
--- app/services/company/scrapper/manager.py
+++ app/services/company/scrapper/manager.py
@@ -6,11 +6,14 @@ from libs.logger import BASE_LOGGER
 
 LOGGER = BASE_LOGGER.getChild(__name__)
 
+
+@timeout_decorator.timeout(10)
 def scrap_company_list(url: str, col: list):
     try:
         df = pd.read_csv(url, usecols=col)
-    except:
+    except Exception as e:
         df = []
+        LOGGER.info(f"Ticker {url} not working, error: {e}")
     return df
 
 def manager(state: State):
@@ -19,15 +22,11 @@ def manager(state: State):
     url_amex = "https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download"
     url_nyse = "https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download"
     col = ["Symbol", "Name", "MarketCap", "IPOyear", "Sector", "industry"]
-    stock_exchange = ["nasdaq", "amex", "nyse"]
 
 
-
-    # @timeout_decorator.timeout(5)
-    LOGGER.info("Starting all .csv files")
-    for market in stock_exchange:
-        LOGGER.info(f"{market}")
-        state.files.market = scrap_company_list(url="url_"+market)
+    state.files.nasdaq = scrap_company_list(url= url_nasdaq, col = col)
+    state.files.amex = scrap_company_list(url=url_amex, col=col)
+    state.files.nyse = scrap_company_list(url=url_nyse, col=col)
     LOGGER.info("Reading all .csv files")
     # exit()
     # df_amex = pd.read_csv(url_amex, usecols=col)
