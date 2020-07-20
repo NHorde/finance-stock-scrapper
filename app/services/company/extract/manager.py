1910fc363bc21fa72886ce510da0b5336868db8d
NHorde <enhorde@gmail.com>
2020-07-19 23:45:29 -0400

    Adding docstring for extract

--- app/services/company/extract/manager.py
+++ app/services/company/extract/manager.py
@@ -9,22 +9,38 @@ import urllib.request
 LOGGER = BASE_LOGGER.getChild(__name__)
 
 
-@timeout_decorator.timeout(10)
+timeout_decorator.timeout(10)
 def scrap_company_list(url: str, path: str):
+    """
+    Download the list of all company tickers and store it in the app/data folder
+
+    :param url: string
+    :param path: string
+    :return:
+    """
     try:
-        df = urllib.request.urlretrieve(url, path + "/data/ticker_list.csv")
+        urllib.request.urlretrieve(url, path + "/data/ticker_list.csv")
     except Exception as e:
-        df = []
         LOGGER.warning(f"URL {url} not working, error: {e}")
-    return df
 
 
 def extract(state: State):
+    """
+    Call extract function
+
+    :param state: state
+    :return: Status of the extract
+    """
     url = "https://dumbstockapi.com/stock?format=csv&countries=US"
     scrap_company_list(url=url, path=PATH)
     return status_extract(state=state)
 
 def status_extract(state: State):
+    """
+    Return 100 if extract was successful, remain as 400 by default otherwise
+    :param state: string
+    :return: dataframe
+    """
     state.events.extract_company_list = 100
 
 
