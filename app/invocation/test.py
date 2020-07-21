13f7514dc0f150bf08f117daa1742e42565976e7
NHorde <enhorde@gmail.com>
2020-07-21 08:16:25 -0400

    Debugging when ticker is incorrect

--- app/invocation/test.py
+++ app/invocation/test.py
@@ -17,6 +17,27 @@
 # print(df.head())
 
 # u = "https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download"
-v = "https://dumbstockapi.com/stock?format=csv&countries=US"
-import urllib.request
-urllib.request.urlretrieve (v, "Ktest.csv")
+# v = "https://dumbstockapi.com/stock?format=csv&countries=US"
+# import urllib.request
+# urllib.request.urlretrieve (v, "Ktest.csv")
+
+
+# import requests
+#
+# url = "https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download"
+# url2 = "https://www.nasdaq.com/market-activity/stocks/screener?exchange=nasdaq&letter=0&render=download"
+# r = requests.get(url2)
+# with open('a.csv', 'wb') as f:
+#     f.write(r.content)
+
+# NYSE
+url_nyse = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download"
+# Nasdaq
+url_nasdaq = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download"
+# AMEX
+url_amex = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download"
+
+import pandas as pd
+
+df = pd.read_csv(url_nyse)
+stocks = df.index.tolist()
